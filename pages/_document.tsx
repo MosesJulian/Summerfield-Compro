9363994b46236b87b5184a99f5ef3a599bb383c8
MossPl <moses.x2rpl21@cintakasihtzuchi.sch.id>
2023-05-11 10:53:06 +0700

    chore: move head to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document';
 const Document = () => {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head title="Summerfield">
+				<link rel="icon" href="/summerfield.ico" />
+				<link
+					rel="preload"
+					href="/fonts/Inter-roman.var.woff2?v=3.19"
+					as="font"
+					type="font/woff2"
+					crossOrigin="anonymous"
+				/>
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import Head from 'next/head';
 import Header from '@/components/Header';
 import Introduction from '@/components/Introduction';
 import About from '@/components/About';
@@ -12,18 +10,6 @@ const inter = Inter({ subsets: ['latin'] });
 const Home: NextPage = () => {
 	return (
 		<>
-			<Head>
-				<title>Summerfield</title>
-				<link rel="icon" href="/summerfield.ico" />
-				<link
-					rel="preload"
-					href="/fonts/Inter-roman.var.woff2?v=3.19"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-			</Head>
-
 			<Header />
 
 			<div className="flex flex-col">
