cd6443c54453a9280a0a3698e1effb331ddba830
Moses Julian <mosesjul1425@gmail.com>
2023-07-11 20:14:19 +0700

    fix: metadata for description change because of wrong location

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ const Document = () => {
 			<Head title="Summerfield Homestay & Cafe">
 				<meta
 					name="description"
-					content="Summerfield Homestay & Cafe is a homestay located in the heart of Bandung, Indonesia. We offer a wide range of rooms and amenities to make your stay as comfortable as possible."
+					content="Summerfield Homestay & Cafe is a homestay located in Watukarung, Yogyakarta, Indonesia. We offer a wide range of rooms and amenities to make your stay as comfortable as possible. We ensure your stay is unforgettable and relaxing."
 				/>
 				<meta
 					name="keywords"
@@ -16,7 +16,7 @@ const Document = () => {
 				<meta name="og:title" content="Summerfield Homestay & Cafe" />
 				<meta
 					name="og:description"
-					content="Summerfield Homestay & Cafe is a homestay located in the heart of Bandung, Indonesia. We offer a wide range of rooms and amenities to make your stay as comfortable as possible."
+					content="Summerfield Homestay & Cafe is a homestay located in Watukarung, Yogyakarta, Indonesia. We offer a wide range of rooms and amenities to make your stay as comfortable as possible. We ensure your stay is unforgettable and relaxing."
 				/>
 				<meta name="og:type" content="website" />
 				<meta name="og:url" content="https://summerfield.vercel.app/" />
